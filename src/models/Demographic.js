49b57c11a2c7887a9ba9eeb3b038809f879fb698
NemanjaDev <nemanja@xola.com>
2018-09-21 15:26:50 +0200

    XOL-4174 Adding support for demographic icons

--- src/models/Demographic.js
+++ src/models/Demographic.js
@@ -3,7 +3,23 @@ import { BaseModel } from "../BaseModel";
 export const Demographic = BaseModel.extend({
     urlRoot: "/demographics",
 
+    demographicsMap: {
+        children: ['child', 'children', 'junior'],
+        adult: ['adults', 'adult', 'guest', 'guests'],
+        senior: ['senior', 'seniors'],
+        veteran: ['military', 'veteran', 'veterans', 'soldier', 'soldiers']
+    },
+
     getLabel() {
         return this.get("label");
-    }
+    },
+
+    guessDemographicIcon: function(label) {
+        label = label || this.get('label');
+        label = label && label.toLowerCase();
+        var icon = _.findKey(this.demographicsMap, function(list) {
+            return _.contains(list, label);
+        });
+        return icon || 'adult';
+    },
 });
